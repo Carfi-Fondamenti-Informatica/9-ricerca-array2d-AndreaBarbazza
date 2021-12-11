eca91288d072f63ddbcb8c101beb60a7bbfee3ba
AndreaBarbazza <91791228+AndreaBarbazza@users.noreply.github.com>
2021-12-11 17:50:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+#include <cstring>
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    char lista[10][20];
+    char test [1][20];
+    int pos;
+    for (int i=0; i<10; i++){
+        cin >> lista[i];
+    }
+
+    cin >> test[0];
+    if (ricerca_nomi(lista, test, pos)){
+        cout << pos << endl;
+    }else{
+        cout << "non presente"<< endl;
+    }
+
+    return 0;
 }
