b87919cf10ebacbaad30ded7317597bbd9c793e2
AndreaBarbazza <91791228+AndreaBarbazza@users.noreply.github.com>
2021-12-11 17:47:42 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+#include "lib.h"
+#include <iostream>
+#include <cstring>
 
+
+bool ricerca_nomi(char lista[10][20], char nome[1][20], int &posizione){
+
+    for (int i=0; i<10; i++){
+        if (strcmp(lista[i],nome[0])==0){
+            posizione=i;
+            return true;
+        }
+
+
+    }
+
+    return false;
+
+
+
+}
